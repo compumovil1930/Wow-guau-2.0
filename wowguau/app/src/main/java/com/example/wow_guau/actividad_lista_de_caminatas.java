0bf1d86cc201dd7edb10cc15b55b0b3671a40f95
TheCodingJuan <juancamachoc97@gmail.com>
2019-08-26 16:43:02 -0500

    Adicion de 3 Actividades: Lista de Caminatas, Solicitud de Caminata, Solicitud de paseo para paseador

---
+++ wowguau/app/src/main/java/com/example/wow_guau/actividad_lista_de_caminatas.java
@@ -0,0 +1,15 @@
+package com.example.wow_guau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class actividad_lista_de_caminatas extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_lista_de_caminatas);
+    }
+}
