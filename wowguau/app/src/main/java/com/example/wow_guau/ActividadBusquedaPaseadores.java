849f2aa682bc0f9568156aee5ddec50bf9eae06d
Santiago <p-lizarazo@javeriana.edu.co>
2019-08-26 12:39:13 -0500

    Pantalla Busqueda de Paseador

---
+++ wowguau/app/src/main/java/com/example/wow_guau/ActividadBusquedaPaseadores.java
@@ -0,0 +1,14 @@
+package com.example.wow_guau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadBusquedaPaseadores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_busqueda_paseadores);
+    }
+}
--- wowguau/app/src/main/java/com/example/wow_guau/Login.java
+++ wowguau/app/src/main/java/com/example/wow_guau/Login.java
@@ -2,7 +2,10 @@ package com.example.wow_guau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn = findViewById(R.id.button_login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext() , ActividadPerfilPaseador.class);
+                startActivity(i);
+            }
+        });
     }
 }
