01afc3220539f09886d9d261246221cff0f30847
Santiago <p-lizarazo@javeriana.edu.co>
2019-08-26 10:42:21 -0500

    Pantalla De Inicio

---
+++ wowguau/app/src/main/java/com/example/wow_guau/ActividadPantallaInicio.java
@@ -0,0 +1,14 @@
+package com.example.wow_guau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActividadPantallaInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividad_pantalla_inicio);
+    }
+}
