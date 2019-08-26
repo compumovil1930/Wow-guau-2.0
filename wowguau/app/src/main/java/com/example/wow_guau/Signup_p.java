391619093c0e4a018962acd7d604ec9d1c3b4abe
Vale <valyatev@gmail.com>
2019-08-26 08:06:58 -0500

    p_recovery sign_up main

---
+++ wowguau/app/src/main/java/com/example/wow_guau/Signup_p.java
@@ -0,0 +1,14 @@
+package com.example.wow_guau;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Signup_p extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_p);
+    }
+}
