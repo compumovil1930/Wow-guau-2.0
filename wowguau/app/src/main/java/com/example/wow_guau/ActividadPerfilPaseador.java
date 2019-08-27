d5e826314d8f4cd5677406ec02c888dea7f27bbe
Santiago <p-lizarazo@javeriana.edu.co>
2019-08-26 19:45:10 -0500

    Actividad Comentarios Paseador y mejoras en landscape

--- wowguau/app/src/main/java/com/example/wow_guau/ActividadPerfilPaseador.java
+++ wowguau/app/src/main/java/com/example/wow_guau/ActividadPerfilPaseador.java
@@ -2,7 +2,10 @@ package com.example.wow_guau;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActividadPerfilPaseador extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class ActividadPerfilPaseador extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_perfil_paseador);
+
+        Button btn = findViewById(R.id.ver_calificaciones_btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ActividadComentariosPaseador.class);
+                startActivity(i);
+            }
+        });
     }
 }
