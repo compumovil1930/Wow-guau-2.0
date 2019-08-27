88d720573841000d1c0cad34db97b57c9a4af19d
Santiago <p-lizarazo@javeriana.edu.co>
2019-08-26 23:23:33 -0500

    Actividad Monitorear Mascota y mejoras en landscape

--- wowguau/app/src/main/java/com/example/wow_guau/ActividadPantallaInicio.java
+++ wowguau/app/src/main/java/com/example/wow_guau/ActividadPantallaInicio.java
@@ -15,6 +15,14 @@ public class ActividadPantallaInicio extends AppCompatActivity {
         setContentView(R.layout.activity_actividad_pantalla_inicio);
 
         Button btn = findViewById(R.id.buscar_paseadores_btn);
+        Button btn_monitoriar = findViewById(R.id.monitoriar_perro_btn);
+        btn_monitoriar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ActividadSeleccionMascotaMonitorear.class);
+                startActivity(i);
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
