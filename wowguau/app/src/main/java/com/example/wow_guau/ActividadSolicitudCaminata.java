01aa4e8e76b010995da6b7e962440fbd7adeae3a
TheCodingJuan <juancamachoc97@gmail.com>
2019-08-27 16:36:38 -0500

    Adicion de 3 Actividades: Lista de Caminatas, Solicitud de Caminata, Solicitud de paseo para paseador

--- wowguau/app/src/main/java/com/example/wow_guau/ActividadSolicitudCaminata.java
+++ wowguau/app/src/main/java/com/example/wow_guau/ActividadSolicitudCaminata.java
@@ -3,12 +3,27 @@ package com.example.wow_guau;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ActividadSolicitudCaminata extends AppCompatActivity {
-
+public class ActividadSolicitudCaminata extends AppCompatActivity
+{
+    Button btnSolicitud;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_solicitud_caminata);
+
+        btnSolicitud=findViewById(R.id.btnSolicitar);
+        btnSolicitud.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+              Toast.makeText(getApplicationContext(),"La solicitud ha sido aprobada",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
