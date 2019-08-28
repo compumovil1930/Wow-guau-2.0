65c28d8f146ffef0b1a0c672b2e92a336c5e695c
TheCodingJuan <juancamachoc97@gmail.com>
2019-08-27 21:02:07 -0500

    Merge branch 'master' of https://github.com/compumovil1930/Wow-guau-2.0
    # Conflicts:
    #	wowguau/app/src/main/AndroidManifest.xml

--- wowguau/app/src/main/java/com/example/wow_guau/ActividadListaCaminatas.java
+++ wowguau/app/src/main/java/com/example/wow_guau/ActividadListaCaminatas.java
@@ -5,17 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ActividadListaCaminatas extends AppCompatActivity
 {
     TextView txtCaminata;
+    ImageButton imgBtnLocation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_lista_de_caminatas);
 
         txtCaminata = findViewById(R.id.txtViewMap2);
+        imgBtnLocation = findViewById(R.id.imgBtnLocation);
 
         txtCaminata.setOnClickListener(new View.OnClickListener()
         {
@@ -23,6 +26,16 @@ public class ActividadListaCaminatas extends AppCompatActivity
             public void onClick(View view)
             {
                 Intent i= new Intent(getApplicationContext(),ActividadSolicitudCaminata.class);
+                startActivity(i);
+            }
+        });
+
+        imgBtnLocation.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Intent i = new Intent(getApplicationContext(),ActividadSolicitudPaseador.class);
                 startActivity(i);
             }
         });
