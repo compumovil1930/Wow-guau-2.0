0dd3048e41ec316c3868856b8ff46d10c026c697
Laura Donado <ldonado11@gmail.com>
2019-10-01 21:34:21 -0500

    Create Permisos.java

---
+++ wowguau/app/src/main/java/com/example/wow_guau/utils/Permisos.java
@@ -0,0 +1,45 @@
+package com.example.wow_guau.utils;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.example.wow_guau.Signup_walker;
+
+public class Permisos {
+    public final static int MY_PERMISSIONS_REQUEST_READ_STORAGE = 1;
+    public final static int MY_PERMISSIONS_REQUEST_CAMERA = 2;
+    public final static int REQUEST_IMAGE_CAPTURE = 3;
+    public final static int IMAGE_PICKER_REQUEST = 4;
+
+    public static void requestPermission(Activity context, String permiso, String justificacion, int idCode) {
+        if (ContextCompat.checkSelfPermission(context, permiso) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)) {
+                Toast.makeText(context, justificacion, Toast.LENGTH_LONG).show();
+            }
+            ActivityCompat.requestPermissions(context, new String[]{permiso}, idCode);
+        }
+    }
+/*    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults){
+        switch (requestCode){
+            case MY_PERMISSIONS_REQUEST_READ_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    pickImage();
+                }else{
+
+                }
+                return;
+            case MY_PERMISSIONS_REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    takePicture();
+                }else{
+
+                }
+                return;
+        }
+    }*/
+}
