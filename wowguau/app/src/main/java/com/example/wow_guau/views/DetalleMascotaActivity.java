d7e6195829bf05fa708bccb69d792a60fe4d3718
Valentina Yate <valyatev@gmail.com>
2019-10-06 12:19:39 -0500

    creada actividad detalle mascota

---
+++ wowguau/app/src/main/java/com/example/wow_guau/views/DetalleMascotaActivity.java
@@ -0,0 +1,17 @@
+package com.example.wow_guau.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.wow_guau.R;
+
+public class DetalleMascotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_mascota);
+
+    }
+}
