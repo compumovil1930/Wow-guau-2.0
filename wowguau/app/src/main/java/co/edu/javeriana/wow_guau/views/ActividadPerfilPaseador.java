a8dff10e61ffabb107179d9824886f8e3ca0bf0a
Adrian Garcia <adriangarcia@javeriana.edu.co>
2019-10-18 23:27:38 -0500

    edición de perfil de paseador

--- wowguau/app/src/main/java/co/edu/javeriana/wow_guau/views/ActividadPerfilPaseador.java
+++ wowguau/app/src/main/java/co/edu/javeriana/wow_guau/views/ActividadPerfilPaseador.java
@@ -16,7 +16,7 @@ public class ActividadPerfilPaseador extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_actividad_perfil_paseador);
 
-        Button btn = findViewById(R.id.ver_calificaciones_btn);
+        Button btn = findViewById(R.id.btnVerCalificacionesPaseador);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
