4273cdedd39f06b27f1fe47ae0c3ecdd6dbfad5d
TheCodingJuan <juancamachoc97@gmail.com>
2019-10-22 15:42:47 -0500

    Perfil Mascota

--- wowguau/app/src/main/java/co/edu/javeriana/wow_guau/views/ListaMascotasActivity.java
+++ wowguau/app/src/main/java/co/edu/javeriana/wow_guau/views/ListaMascotasActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -67,6 +68,7 @@ public class ListaMascotasActivity extends AppCompatActivity
 
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recycleView.setLayoutManager(mLayoutManager);
+
         recycleView.setItemAnimator(new DefaultItemAnimator());
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitNetwork().build();
